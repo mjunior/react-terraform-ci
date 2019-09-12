9e0a123009cd4d762ee640e4146d459e65149608
mjunior <mauriciojs.junior@gmail.com>
2019-09-12 17:01:05 -0300

    Mensagem inicial

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          vrau.com.br - from terraform2
+          novo texto from ci2
         </p>
         <a
           className="App-link"
